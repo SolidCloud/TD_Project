c2b36960ac74039b1b297af31e511440c40458cf
Christopher Åkersten <christopher.akersten@gmail.com>
2012-12-08 05:54:00 +0100

    Added comp. method

--- src/se/chalmers/chraker/johlin/TD_Project/CPosition.java
+++ src/se/chalmers/chraker/johlin/TD_Project/CPosition.java
@@ -1,6 +1,6 @@
 package se.chalmers.chraker.johlin.TD_Project;
 
-public class CPosition {
+public class CPosition{
 	private int x;
 	private int y;
 	public CPosition( int x, int y){
@@ -12,5 +12,9 @@ public class CPosition {
 	}
 	public int getY(){
 		return y;
+	}
+	public boolean closerThanToTarget (CPosition compare, CPosition target){
+		return Math.sqrt(Math.pow(target.getX()-this.getX(),2) + Math.pow(target.getY()-this.getY(),2)) >
+			Math.sqrt(Math.pow(target.getX()-compare.getX(),2) + Math.pow(target.getY()-compare.getY(),2));
 	}
 }
--- src/se/chalmers/chraker/johlin/TD_Project/ProjectileTurret.java
+++ src/se/chalmers/chraker/johlin/TD_Project/ProjectileTurret.java
@@ -2,6 +2,7 @@ package se.chalmers.chraker.johlin.TD_Project;
 
 import java.awt.Graphics;
 import java.awt.geom.Area;
+import java.util.ArrayList;
 
 public class ProjectileTurret extends Turret {
 	private Projectile toBeFired;
@@ -14,7 +15,38 @@ public class ProjectileTurret extends Turret {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Projectile getProjectile() {
+	private Unit getTarget(Unit[] units) {
+		if(cooldownRemaining <= 0){	
+			ArrayList<Unit> targetArray = new ArrayList ();
+			for(int unitIndex = 0; unitIndex < units.length; unitIndex++){
+				Area checkIntersect = (Area)range.clone();
+				checkIntersect.intersect(units[unitIndex]);
+				if (checkIntersect.isEmpty()){
+					targetArray.add(units[unitIndex]);
+				}
+			}
+			switch (targetMode){
+			case CLOSE:
+				Area temp;
+				for(int unitIndex = 0; unitIndex < targetArray.size(); unitIndex++ ){
+					if()
+				}
+				break;
+			case FAR:
+				break;
+			case FIRST:
+				break;
+			case LAST:
+				break;
+			case STRONG:
+				break;
+			case WEAK:
+				break;
+			}
+		}
+		return null;
+	}
+	private void fire() {
 		
 	}
 	
