b9711bed35c9947d4c3771ecd0f05d7b4ea810da
Christopher Åkersten <christopher.akersten@gmail.com>
2012-12-12 13:36:49 +0100

    Mayor update
    - Several methds removed
    - Functionality unique to subclass moved to subclass (target mode)

--- src/se/chalmers/chraker/jolinds/tdproject/Turret.java
+++ src/se/chalmers/chraker/jolinds/tdproject/Turret.java
@@ -7,30 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Turret extends EconomyStructure {
-	public enum TargetMode {
-		FIRST, LAST, WEAK, STRONG, CLOSE, FAR;
-		public TargetMode toggle() {
-			switch(this) {
-				case CLOSE:
-					return FAR;
-				case FAR:
-					return CLOSE;
-				case FIRST:
-					return LAST;
-				case LAST:
-					return FIRST;
-				case STRONG:
-					return WEAK;
-				case WEAK:
-					return STRONG;
-				default:
-					return FIRST;
-			}
-		}
-	}
 
 	private Area range;
-	private TargetMode targetMode;
 	private int cooldown;
 	protected int cooldownRemaining;
 	
@@ -51,64 +29,28 @@ public class Turret extends EconomyStructure {
 		this.cooldown = cooldown;
 		this.cooldownRemaining = cooldown;
 		this.range = range;
-		targetMode = TargetMode.FIRST;
 	}
-	
-	public TargetMode getTargetMode() {
-		return targetMode;
-	}
-	
 	public Area getRange() {
 		return (Area) range.clone();
 	}
 	
-	public void setTargetMode(TargetMode targetMode) {
-		this.targetMode = targetMode;
-	}
 	
-	private Area getTarget(List<Unit> units) {
+	protected List<Unit> getTargets(List<Unit> units) {
 		List<Unit> targets = new ArrayList<Unit>();
-		if(cooldownRemaining <= 0){	
-			for (Unit unit : units){
-				Area checkIntersect = getRange();
-				checkIntersect.intersect(unit.getBounds());
-				if (checkIntersect.isEmpty()){
-					targets.add(unit);
-				}
+		for (Unit unit : units){
+			Area checkIntersect = getRange();
+			checkIntersect.intersect(unit.getBounds());
+			if (!checkIntersect.isEmpty()){
+				targets.add(unit);
 			}
-			switch (getTargetMode()){
-			case CLOSE:
-				Area temp;
-				for(Unit target : targets){
-					//if()
-					// TODO something is not finished here
-				}
-				break;
-			case FAR:
-				break;
-			case FIRST:
-				break;
-			case LAST:
-				break;
-			case STRONG:
-				break;
-			case WEAK:
-				break;
-			}
-
 		}
-		return null;
+		return targets;
 	}
-	public boolean checkFire(int elapsedTime, Area[] units) {
-		return false;
+	protected void fire(){
+		this.cooldownRemaining = cooldown;
 	}
-
-	private void fire() {
-		
-	}
-	
 	@Override
 	public void update(int elapsedTime){
-		cooldownRemaining -= elapsedTime;
+		cooldownRemaining = GameUtils.modSub(cooldownRemaining, elapsedTime);
 	}
 }
